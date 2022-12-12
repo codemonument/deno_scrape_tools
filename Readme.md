0b0534cec8bad5fff2fc2e78d0d5a99ddd770d73
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-12 02:12:47 +0100

    add puppeteer as dependency

--- Readme.md
+++ Readme.md
@@ -3,4 +3,12 @@
 
 A deno library of tools useful to writing scripts scraping websites
 
+## Dependencies
+
+- https://deno.land/x/puppeteer@9.0.2/ 
+  (
+   bc. this lib is extracted from an existing project 
+   and I don't want to break too much at once 
+   by extracting and upgrading at the same time'
+  )
 
---
+++ deps/puppeteer.ts
@@ -0,0 +1,17 @@
+// TODO: Upgrade to newest one!
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
+
+/*
+Puppeteer can use any recent version of Chromium or Firefox Nightly,
+but this version of Puppeteer is only validated against a specific version.
+To cache these versions in the Puppeteer cache, run the commands below.
+
+PUPPETEER_PRODUCT=chrome deno run -A --unstable https://deno.land/x/puppeteer@16.2.0/install.ts
+PUPPETEER_PRODUCT=firefox deno run -A --unstable https://deno.land/x/puppeteer@16.2.0/install.ts
+
+You can find all of the supported environment variables to customize installation in the Puppeteer docs at
+https://pptr.dev/#?product=Puppeteer&version=v16.2.0&show=api-environment-variables
+*/
+
+export * from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
+export { default } from "https://deno.land/x/puppeteer@9.0.2/mod.ts";

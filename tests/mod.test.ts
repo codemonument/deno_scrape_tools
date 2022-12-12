5180f7dd39a4526390b40b99d583ea93031d811c
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-12 02:25:04 +0100

    add permission flag to mod.test (WIP)

--- tests/mod.test.ts
+++ tests/mod.test.ts
@@ -1,7 +1,8 @@
 import { assert, describe, it } from "@testing";
 import { Puppeteer } from "@mod";
 
-describe(`mod.ts`, () => {
+// TODO: Debug why this permissions flag is not working as thought - deno still asks for permission
+describe(`mod.ts`, { permissions: { env: ["PUPPETEER_PRODUCT"] } }, () => {
   it(`should export correct objects and types`, () => {
     assert(Puppeteer);
   });
